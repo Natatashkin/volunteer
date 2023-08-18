2090ee09649e40e8e584b0678ab1dbb33987deac
Natatashkin <cs070283sng@gmail.com>
2023-08-18 14:12:24 +0300

    finish middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,35 @@ import { match } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
 import { NextRequest, NextResponse } from "next/server";
 
-let locales = ["en", "uk"];
-let defaultLocale = "uk";
+const getAllLocales = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/i18n/locales`,
+    {
+      headers: {
+        Authorization: `bearer ${process.env.NEXT_PUBLIC_STRAPI_API_TOKEN}`,
+      },
+    }
+  );
+  const data = await res.json();
+  return data;
+};
 
-const getLocale = (request: NextRequest) => {
+const fetchLocalesData = async () => {
+  const allLocales = await getAllLocales();
+  const locales = allLocales.map(({ code }: { code: string }) => code);
+  const defaultLocale =
+    allLocales.find(({ isDefault }: { isDefault: boolean }) => isDefault)
+      ?.code || "en";
+
+  return {
+    locales,
+    defaultLocale,
+  };
+};
+
+const getLocale = async (request: NextRequest) => {
+  const { locales, defaultLocale } = await fetchLocalesData();
+
   // Negotiator expects plain object so we need to transform headers
   const negotiatorHeaders: Record<string, string> = {};
   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
@@ -14,17 +39,19 @@ const getLocale = (request: NextRequest) => {
   return match(languages, locales, defaultLocale);
 };
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
+  const { locales } = await fetchLocalesData();
   // Check if there is any supported locale in the pathname
   const pathname = request.nextUrl.pathname;
   const pathnameIsMissingLocale = locales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+    (locale: string) =>
+      !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
   );
 
   // Redirect if there is no locale
   if (pathnameIsMissingLocale) {
-    const locale = getLocale(request);
-    console.log(locale);
+    const locale = await getLocale(request);
+    // console.log(locale);
 
     // e.g. incoming request is /products
     // The new URL is now /en-US/products
